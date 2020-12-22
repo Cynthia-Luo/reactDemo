8be63aab5996fbe5a439d1381f7d7c490b124301
Cynthia <cynthialuo999@gmail.com>
2020-12-22 15:28:26 +1100

    init

---
+++ src/App.js
@@ -0,0 +1,17 @@
+/* 
+Root component
+*/
+
+import React, { Component } from 'react'
+
+
+export default class App extends Component {
+
+    render () {
+        return(
+            <div>App</div>
+        )
+    }
+
+    
+}
