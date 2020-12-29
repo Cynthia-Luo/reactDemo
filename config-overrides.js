d9b949e6a344c4fabeb08d8f036f3aa862a8728d
Cynthia <cynthialuo999@gmail.com>
2020-12-29 19:05:18 +1100

    Save before ejecting

--- config-overrides.js
+++ config-overrides.js
@@ -1,31 +1,22 @@
-const {override, fixBabelImports, addLessLoader} = require('customize-cra');
+ const { override, fixBabelImports } = require('customize-cra');
 
 
+ module.exports = override(
+   fixBabelImports('import', {
+     libraryName: 'antd',
+     libraryDirectory: 'es',
+     style: 'css',
+   }),
 
-module.exports = override(
+  //  addLessLoader({
+  //   lessOptions: {
+  //     javascriptEnabled: true,
+  //     modifyVars: { '@primary-color': '#1DA57A' },
+  //   },
+  // }),
+  // addLessLoader({
+  //   javascriptEnabled: true,
+  //   modifyVars: { '@primary-color': '#1DA57A' },
+  // }),
+ );
 
-  
-    fixBabelImports('import', { 
-        libraryName: 'antd', 
-        libraryDirectory: 'es', 
-        style: true, }
-    ), 
-        
-    // addLessLoader({
-    //     lessOptions: {
-    //       javascriptEnabled: true,
-    //       modifyVars: { '@primary-color': '#1DA57A' },
-    //     },
-    //   }),
-    addLessLoader({ 
-        javascriptEnabled: true, 
-        modifyVars: {'@primary-color': '#1DA57A'}, 
-    
-    
-    }), 
-
-    addLessLoader({ 
-    loader: 'less-loader',
-    options: { javascriptEnabled: true } ,
-    }), 
-);
